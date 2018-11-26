61c9c3ba8441bfe78b6d12f20519eaa19f33106b
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-11-26 10:45:07 -0400

    Added missing files for starting project

---
+++ src/main/java/edu/cooking/recipes/RecipesApplication.java
@@ -0,0 +1,12 @@
+package edu.cooking.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RecipesApplication.class, args);
+  }
+}
