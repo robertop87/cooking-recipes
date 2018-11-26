76a01269a0a70e3d55b287f28038ca9a00f96a84
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-11-26 14:31:49 -0400

    Updated Test to test sending a UserEntry as JSON

--- src/test/java/edu/cooking/recipes/UsersApiTests.java
+++ src/test/java/edu/cooking/recipes/UsersApiTests.java
@@ -1,13 +1,14 @@
 package edu.cooking.recipes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.cooking.recipes.application.users.UserEntry;
-import lombok.val;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,15 +22,20 @@ public class UsersApiTests {
   @Autowired
   private MockMvc mockMvc;
 
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
   @Test
   public void testRegisterAnUserShouldReturnsCreatedStatus() throws Exception {
-    val user = UserEntry.builder()
+    UserEntry user = UserEntry.builder()
         .fullName("Test User")
         .birthInDdMmYy("17-09-2017")
         .email("test.user@email.com")
-        .password("Password@123");
+        .password("Password@123")
+        .build();
 
-    this.mockMvc.perform(MockMvcRequestBuilders.post("/users", user))
+    this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(this.objectMapper.writeValueAsString(user)))
         .andExpect(MockMvcResultMatchers.status().isCreated());
   }
 }
