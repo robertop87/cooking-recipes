d3b84ebd686a306487b241e4c9fbb1b482f1165a
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2018-11-26 14:35:08 -0400

    Updated UserController to accept and validate UserEntry

--- src/main/java/edu/cooking/recipes/presentation/UserController.java
+++ src/main/java/edu/cooking/recipes/presentation/UserController.java
@@ -1,15 +1,31 @@
 package edu.cooking.recipes.presentation;
 
-import java.net.URI;
+import edu.cooking.recipes.application.users.UserEntry;
+import edu.cooking.recipes.application.users.UserService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class UserController {
 
+  @Autowired
+  private UserService service;
+
   @PostMapping("/users")
-  public ResponseEntity<Void> registeUser() {
-    return ResponseEntity.created(URI.create("test")).build();
+  @ResponseBody
+  public ResponseEntity<Void> registeUser(UriComponentsBuilder uriComponentsBuilder,
+      @RequestBody @Valid UserEntry userEntry) {
+
+    UriComponents uriComponents = uriComponentsBuilder.path("/users/{id}")
+        .buildAndExpand(this.service.registerUser(userEntry));
+
+    return ResponseEntity.created(uriComponents.toUri()).build();
   }
 }
